c2b2d1ed26a6bc41fa3f250b883c5a16dab0a0fc
mc-iutsm <maxime.coutteau@univ-rennes.fr>
2025-09-26 12:01:06 +0200

    Update IF_exemple.sh

--- Correction/IF_exemple.sh
+++ Correction/IF_exemple.sh
@@ -1,1 +1,8 @@
-
+#!/bin/bash
+read -p "entrer une valeur numérique :" mavar 
+if [ $mavar -ge 10 ] 
+then 
+  echo "mavar est plus grand ou égal à 10"
+else 
+  echo "mavar est plus petit que 10"
+fi
